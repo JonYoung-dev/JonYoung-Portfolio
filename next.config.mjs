11f0946611a0d9185643843fbed596702bcce086
J-Young <jyoung222@my.bcit.ca>
2025-10-31 12:45:50 -0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  // can define acceptable addresses here to navigate around cors errors
   reactCompiler: true,
 };
 
